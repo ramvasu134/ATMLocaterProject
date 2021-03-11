7635e959f3f28bddb78310285246dc6a425df0b2
Ramakanth Yadav <ramvasu134@gmail.com>
2021-03-11 15:49:29 +0530

    intial checkin

---
+++ src/main/java/com/atm/locater/atmlocater/AtmlocaterApplication.java
@@ -0,0 +1,13 @@
+package com.atm.locater.atmlocater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmlocaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmlocaterApplication.class, args);
+	}
+
+}
