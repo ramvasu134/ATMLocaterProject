b5aae5450112f5674781c934862842ca210f2c09
ramvasu134 <65658330+ramvasu134@users.noreply.github.com>
2021-03-14 10:18:18 +0530

    Add files via upload

--- src/main/java/com/atm/locater/atmlocater/dao/AtmMainObjectDao.java
+++ src/main/java/com/atm/locater/atmlocater/dao/AtmMainObjectDao.java
@@ -21,5 +21,37 @@ public class AtmMainObjectDao {
 	private String type;
 	@JsonProperty("openingHours")
 	private List<OpeningHours> openingHours;
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public String getDistance() {
+		return distance;
+	}
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+	public String getFunctionality() {
+		return functionality;
+	}
+	public void setFunctionality(String functionality) {
+		this.functionality = functionality;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public List<OpeningHours> getOpeningHours() {
+		return openingHours;
+	}
+	public void setOpeningHours(List<OpeningHours> openingHours) {
+		this.openingHours = openingHours;
+	}
+	
+	
 
 }
