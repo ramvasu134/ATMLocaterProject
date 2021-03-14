f992fee5ff9fedf72431a6c147564e1ad2bf5e2c
ramvasu134 <65658330+ramvasu134@users.noreply.github.com>
2021-03-14 10:15:16 +0530

    Add files via upload

--- src/main/java/com/atm/locater/atmlocater/resttemplate/AtmLocaterRestTemplate.java
+++ src/main/java/com/atm/locater/atmlocater/resttemplate/AtmLocaterRestTemplate.java
@@ -1,8 +1,10 @@
 package com.atm.locater.atmlocater.resttemplate;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -13,23 +15,40 @@ import org.springframework.web.client.RestTemplate;
 import com.atm.locater.atmlocater.dao.AtmMainObjectDao;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Data
+@Slf4j
 public class AtmLocaterRestTemplate {
 
 	@Autowired
 	private RestTemplate restTemplate;
 
-	public List<AtmMainObjectDao> getAtmLocationsFromRestTemplate() throws Exception{
+	List<AtmMainObjectDao> atmMainObjectDaos = new ArrayList<AtmMainObjectDao>();
+
+	public List<AtmMainObjectDao> getAtmLocationsFromRestTemplate(String city) throws Exception {
 
 		String response = restTemplate.getForObject("https://www.ing.nl/api/locator/atms/", String.class);
-        String toBeParsed = response.substring(6, response.length());
-        ObjectMapper objectMapper = new ObjectMapper();
-        AtmMainObjectDao[] mainObjectDaos = objectMapper.readValue(toBeParsed, AtmMainObjectDao[].class);
-        return Arrays.asList(mainObjectDaos);
+		String toBeParsed = response.substring(6, response.length());
+		ObjectMapper objectMapper = new ObjectMapper();
+		AtmMainObjectDao[] mainObjectDaos = objectMapper.readValue(toBeParsed, AtmMainObjectDao[].class);
+		atmMainObjectDaos = Arrays.asList(mainObjectDaos);
+		if (city == null) {
+			return atmMainObjectDaos;
+		} else {
+			atmMainObjectDaos = atmMainObjectDaos.stream()
+					.filter(atmAdresObj -> city.equalsIgnoreCase(atmAdresObj.getAddress().getCity()))
+					.collect(Collectors.toList());
+		}
+
+		return atmMainObjectDaos;
 	}
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.setConnectTimeout(Duration.ofMillis(3000)).setReadTimeout(Duration.ofMillis(3000)).build();
 	}
+
 }
