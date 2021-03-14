2a2e56b95fa3688806a8070c34eb36003696b026
ramvasu134 <65658330+ramvasu134@users.noreply.github.com>
2021-03-14 10:17:22 +0530

    Add files via upload

--- src/main/java/com/atm/locater/atmlocater/controller/ATMLocatorRestController.java
+++ src/main/java/com/atm/locater/atmlocater/controller/ATMLocatorRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.atm.locater.atmlocater.controller.service.AtmLocaterService;
@@ -17,8 +18,8 @@ public class ATMLocatorRestController {
 	private AtmLocaterService atmLocatorService; 
 	
 	@GetMapping(path = "/atm", name = "", produces = "application/json")
-	public List<AtmMainObjectDao> getAtms() throws Exception{
-		return atmLocatorService.getAtmLocations();
+	public List<AtmMainObjectDao> getAtms(@RequestParam(required = false) String city) throws Exception{
+		return atmLocatorService.getAtmLocations(city);
     }
 
 }
